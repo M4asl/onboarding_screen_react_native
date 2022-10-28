86a1cd523d9f8e05576de786a8fd6343c5ebc450
M4asl <mateusz.maslowiec98@gmail.com>
2022-10-28 22:49:22 +0200

    Create onboarding screen

---
+++ src/screens/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import OnBoardingScreen from './OnBoardingScreen/OnBoardingScreen';
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <OnBoardingScreen />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
